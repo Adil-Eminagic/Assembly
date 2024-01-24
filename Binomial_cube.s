3f297ebe136c999944753d0d6de8d575dd329806
Adil Eminagic <116492682+Adil-Eminagic@users.noreply.github.com>
2024-01-24 12:07:11 +0100

    Update Binomial_cube.s

--- Binomial_cube.s
+++ Binomial_cube.s
@@ -7,23 +7,23 @@ _start:
 	MOV R8,#0//result
 	
 bin_cube:
-  MUL R7,R0,R0
+  	MUL R7,R0,R0
 	MUL R7,R7,R0
 	ADD R8,R8,R7//a3
 	
 	MOV R7,#0 
-  MUL R7,R1,R1
+  	MUL R7,R1,R1
 	MUL R7,R7,R1
 	ADD R8,R8,R7//b3
 	
 	MOV R7,#0
-  MUL R7,R0,R0
+  	MUL R7,R0,R0
 	MUL R7,R7,R6
 	MUL R7,R7,R1
 	ADD R8,R8,R7//3 * a * a * b
 	
 	MOV R7,#0
-  MUL R7,R1,R1
+  	MUL R7,R1,R1
 	MUL R7,R7,R6
 	MUL R7,R7,R0
 	ADD R8,R8,R7//3 * a * b * b
