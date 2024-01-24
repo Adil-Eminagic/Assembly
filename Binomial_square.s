73a5705e403e060d152d8acbe461c3470999fcd8
Adil Eminagic <116492682+Adil-Eminagic@users.noreply.github.com>
2024-01-24 12:07:39 +0100

    Update Binomial_square.s

--- Binomial_square.s
+++ Binomial_square.s
@@ -7,14 +7,14 @@ _start:
 	MOV R8,#0//result
 	
 bin_square:
-  MUL R7,R0,R0
+  	MUL R7,R0,R0
 	ADD R8,R8,R7//a2
 	
 	MOV R7,#0 
-  MUL R7,R1,R1
+  	MUL R7,R1,R1
 	ADD R8,R8,R7//b2
 	
 	MOV R7,#0
-  MUL R7,R0,R6
+  	MUL R7,R0,R6
 	MUL R7,R7,R1
 	ADD R8,R8,R7//2ab
